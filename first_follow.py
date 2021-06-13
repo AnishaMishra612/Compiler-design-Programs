56952db3437038cd895c922b79ebaddd218739cf
AnishaMishra612 <68319522+AnishaMishra612@users.noreply.github.com>
2021-06-13 12:37:15 +0530

    Add files via upload

---
+++ first_follow.py
@@ -0,0 +1,63 @@
+from collections import OrderedDict
+def find_substr(s, char):
+    index = 0
+
+    if char in s:
+        c = char[0]
+        for ch in s:
+            if ch == c:
+                if s[index:index+len(char)] == char:
+                    return index
+
+            index += 1
+
+    return -1
+def change_left(substring,key1,gram):
+	return substring.replace(key1,gram)
+def left_recursion(grammar):
+    flag=0
+    pos=0
+    for key in grammar.copy():
+        item = grammar[key]
+        for prod in item:
+            if key == prod[0:1]:
+				#print "left recursion found"
+                flag = 1
+                pos = pos +1
+                subfix = prod[1:len(prod)]
+        if flag == 1:
+            grammar[key] = [item[pos:len(item)][0]+key+"'"]
+            grammar.update({key+"'":["epsln",subfix+key+"'"]})
+            flag=pos=0
+    return grammar
+def first(grammar,terminal):
+	for key in grammar.copy():
+		lis = []
+		for string in grammar.copy()[key]:
+			for key1 in grammar.copy():
+				if key1 in string:
+					pos = find_substr(string,key1)
+					for i in range(len(grammar[key1])):
+						string1 = string[0:pos] + change_left(string[pos:pos+len(key1)],key1,grammar[key1][i]) + string[pos+len(key1):len(string)]
+				else:
+					string1 = string
+			lis.append(string1)
+		grammar.update({key:lis})
+	first1 = {}
+	for key in grammar.copy():
+		firlis = []
+		for ter in terminal:
+			for string in grammar[key]:
+				if string.find(ter)==0:
+					firlis.append(ter)
+		first1.update({key:firlis})
+
+	return first1
+grammar = OrderedDict()
+grammar = {"S":["AaAb","BbBa"],"A":["epsln"],"B":["epsln"]}
+terminal = ["(",")","id","epsln","*","+","a","b"]
+grammar = left_recursion(grammar)
+print(grammar)
+print("----------------------------------------------")
+x = first(grammar,terminal)
+print(x)
